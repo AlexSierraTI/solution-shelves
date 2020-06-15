0da813cdc56ee6a09aaef8357b53e35af151c27d
AlexSierraTI <alexandre.sdo@gmail.com>
2020-06-15 17:13:22 -0300

    carregando shader de um arquivo

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -96,7 +96,6 @@ public:
 
 		m_Shader = SolutionShelves::Shader::Create(vertexSrc, fragmentSrc);
 
-
 		std::string flatColorShaderVertexSrc = R"(
 			#version 330 core
 			
@@ -129,40 +128,7 @@ public:
 		)";
 
 		m_FlatColorShader = SolutionShelves::OpenGLShader::Create(flatColorShaderVertexSrc, flatColorShaderFragmentSrc);
-
-		std::string textureShaderVertexSrc = R"(
-			#version 330 core
-			
-			layout(location = 0) in vec3 a_Position;
-			layout(location = 1) in vec2 a_TextCoord;
-
-			uniform mat4 u_ViewProjection;
-			uniform mat4 u_Transform;
-
-			out vec2 v_TextCoord;
-
-			void main()
-			{
-				v_TextCoord = a_TextCoord;
-				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
-			}
-		)";
-		std::string textureShaderFragmentSrc = R"(
-			#version 330 core
-			
-			layout(location = 0) out vec4 color;
-			
-			in vec2 v_TextCoord;
-
-			uniform sampler2D u_Texture;
-
-			void main()
-			{
-				color = texture(u_Texture, v_TextCoord);
-			}
-		)";
-
-		m_TextureShader = SolutionShelves::OpenGLShader::Create(textureShaderVertexSrc, textureShaderFragmentSrc);
+		m_TextureShader = SolutionShelves::OpenGLShader::Create("assets/shaders/Texture.glsl");
 
 		m_Texture = SolutionShelves::Texture2D::Create("assets/textures/Checkerboard.png");
 		m_LuanaTexture = SolutionShelves::Texture2D::Create("assets/textures/luana.png");
--- SolutionShelves/src/Engine/Renderer/Shader.cpp
+++ SolutionShelves/src/Engine/Renderer/Shader.cpp
@@ -7,6 +7,17 @@
 
 namespace SolutionShelves
 {
+	Ref<Shader> Shader::Create(const std::string& filepath)
+	{
+		switch (Renderer::GetAPI())
+		{
+		case RendererAPI::API::None:      SS_CORE_ASSERT(false, "RendererAPI::None nao suportada!"); return nullptr;
+		case RendererAPI::API::OpenGL:    return std::make_shared<OpenGLShader>(filepath);
+		}
+		SS_CORE_ASSERT(false, "RendererAPI desconhecido!");
+		return nullptr;
+	}
+
 	Ref<Shader> Shader::Create(const std::string& vertexSrc, const std::string& fragmentSrc)
 	{
 		switch (Renderer::GetAPI())
