9f9312329048b383453dc2e07c975ec102664dfe
AlexSierraTI <alexandre.sdo@gmail.com>
2020-06-17 20:02:05 -0300

    comeco de batch rendenring 2D

--- SolutionShelves/src/Engine/Renderer/Buffer.cpp
+++ SolutionShelves/src/Engine/Renderer/Buffer.cpp
@@ -7,6 +7,17 @@
 
 namespace SolutionShelves
 {
+	Ref<VertexBuffer> VertexBuffer::Create(uint32_t size)
+	{
+		switch (Renderer::GetAPI())
+		{
+			case RendererAPI::API::None:      SS_CORE_ASSERT(false, "RendererAPI::None nao suportada!"); return nullptr;
+			case RendererAPI::API::OpenGL:    return CreateRef<OpenGLVertexBuffer>(size);
+		}
+		SS_CORE_ASSERT(false, "RendererAPI desconhecido!");
+		return nullptr;
+	}
+
 	Ref<VertexBuffer> VertexBuffer::Create(float* vertices, uint32_t size)
 	{
 		switch (Renderer::GetAPI())
@@ -18,12 +29,12 @@ namespace SolutionShelves
 		return nullptr;
 	}
 
-	Ref<IndexBuffer> IndexBuffer::Create(uint32_t* indices, uint32_t size)
+	Ref<IndexBuffer> IndexBuffer::Create(uint32_t* indices, uint32_t count)
 	{
 		switch (Renderer::GetAPI())
 		{
 			case RendererAPI::API::None:      SS_CORE_ASSERT(false, "RendererAPI::None nao suportada!"); return nullptr;
-			case RendererAPI::API::OpenGL:    return CreateRef<OpenGLIndexBuffer>(indices, size);
+			case RendererAPI::API::OpenGL:    return CreateRef<OpenGLIndexBuffer>(indices, count);
 		}
 		SS_CORE_ASSERT(false, "RendererAPI desconhecido!");
 		return nullptr;
